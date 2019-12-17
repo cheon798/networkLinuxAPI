9c706c5cc89e8d767d0e7aa42ca787b215aac436
cheon798 <cheon798@gmail.com>
2019-12-17 16:20:42 +0900

    add Process

---
+++ 00.Process/fork_process.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <wait.h>
+
+int 
+main(int argc, char const *argv[])
+{
+    int i;
+    pid_t cpid;
+
+    for (i=0 ; i<3 ; i++)
+    {
+        cpid = fork();
+
+        printf("[%d] PID(%d) PPID(%d)\n", i, getpid(), getppid());
+
+#ifndef OMIT_SWITH
+
+        switch (cpid) {
+            case 0:         // Child Process
+                pause();
+                break;
+            
+            case -1:        // Error Case
+                break;
+
+            default:        // Parent Process
+                break;
+        }
+#endif
+    }
+
+    wait(NULL);
+
+    return 0;
+}
